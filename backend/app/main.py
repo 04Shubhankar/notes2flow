6f1f6fb94d4890b585fca02b7b35ec61296d11f4
Shubhankar Bhide <109309785+04Shubhankar@users.noreply.github.com>
2026-05-02 23:15:41 +0530

    Added domain to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Notes to Flow")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "https://notes2flow.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
